1af5521aacecf38b59e4b49fd1327b43bd6b8841
Kishore Shiraguppi <kishor4u@gmail.com>
2023-07-27 18:37:33 -0400

    touch events

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById("whiteboard");
-  const context = canvas.getContext("2d");
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('whiteboard');
+  const context = canvas.getContext('2d');
   let isDrawing = false;
-  let penColor = "#000000";
+  let penColor = '#000000';
   let isColorPickerVisible = false;
 
   canvas.width = window.innerWidth - 20;
   canvas.height = window.innerHeight - 120;
 
-  canvas.addEventListener("mousedown", startDrawing);
-  canvas.addEventListener("mousemove", draw);
-  canvas.addEventListener("mouseup", stopDrawing);
+  canvas.addEventListener('mousedown', startDrawing);
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mouseup', stopDrawing);
+  canvas.addEventListener('mouseleave', stopDrawing);
+
+  canvas.addEventListener('touchstart', startDrawing);
+  canvas.addEventListener('touchmove', draw);
+  canvas.addEventListener('touchend', stopDrawing);
 
   function startDrawing(e) {
     isDrawing = true;
@@ -25,13 +30,16 @@ document.addEventListener("DOMContentLoaded", () => {
   function draw(e) {
     if (!isDrawing) return;
 
+    const clientX = e.type === 'touchmove' ? e.touches[0].clientX : e.clientX;
+    const clientY = e.type === 'touchmove' ? e.touches[0].clientY : e.clientY;
+
     context.lineWidth = 2;
-    context.lineCap = "round";
+    context.lineCap = 'round';
     context.strokeStyle = penColor;
 
     const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
 
     context.lineTo(x, y);
     context.stroke();
@@ -39,38 +47,37 @@ document.addEventListener("DOMContentLoaded", () => {
     context.moveTo(x, y);
   }
 
-  const controlsContainer = document.querySelector(".controls");
+  const controlsContainer = document.querySelector('.controls');
 
-  controlsContainer.addEventListener("click", handleControlsClick);
+  controlsContainer.addEventListener('click', handleControlsClick);
 
   function handleControlsClick(e) {
     const target = e.target;
 
-    if (target.id === "colorPickerIcon") {
-    //   e.stopPropagation();
+    if (target.id === 'colorPickerIcon') {
+      e.stopPropagation();
       toggleColorPicker();
-    } else if (target.id === "clearBtn") {
+    } else if (target.id === 'clearBtn') {
       clearCanvas();
     }
   }
 
   function toggleColorPicker() {
     isColorPickerVisible = !isColorPickerVisible;
-    const colorPickerContainer = document.getElementById("colorPicker");
-    colorPickerContainer.style.display = isColorPickerVisible ? "flex" : "none";
-
+    const colorPickerContainer = document.getElementById('colorPicker');
+    colorPickerContainer.style.display = isColorPickerVisible ? 'flex' : 'none';
     if (isColorPickerVisible) {
       const colorInput = document.getElementById("colorInput");
       colorInput.click(); // Trigger the click event on the color input
     }
   }
 
-  const colorConfirm = document.getElementById("colorInput");
-  colorConfirm.addEventListener("change", confirmColor);
+  const colorConfirm = document.getElementById('colorInput');
+  colorConfirm.addEventListener('change', confirmColor);
 
   function confirmColor() {
     penColor = colorConfirm.value;
-    const colorPickerContainer = document.getElementById("colorPicker");
+    const colorPickerContainer = document.getElementById('colorPicker');
     toggleColorPicker();
   }
 
